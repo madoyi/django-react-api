3f155710ae3a1bd34bb9cb08ee69321d60256817
madoyi <brianmadoyi@gmail.com>
2022-02-23 19:35:06 +0300

    chore: react and api intergration

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,9 +11,9 @@ const routing = (
 	<Router>
 		<React.StrictMode>
 			<Header />
-			<Routes>
-			<Route path='/welcome' element={<App/>} />
-			</Routes>
+			<Switch>
+				<Route exact path="/" component={App} />
+			</Switch>
 			<Footer />
 		</React.StrictMode>
 	</Router>
@@ -24,4 +24,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
