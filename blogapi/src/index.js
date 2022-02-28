13b8c99ff82e3acc7e57424e315e9b61b95d2edc
madoyi <brianmadoyi@gmail.com>
2022-02-28 16:55:49 +0300

    nc:viewsets and Routers

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -9,6 +9,7 @@ import Footer from './components/footer';
 import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
+import Single from './components/single';
 
 const routing = (
 	<Router>
@@ -16,9 +17,10 @@ const routing = (
 			<Header />
 			<Switch>
 				<Route exact path="/" component={App} />
-				<Route path="/register" component={Register}/>
-				<Route path="/login" component={Login}/>
-				<Route path="/logout" component={Logout}/>
+				<Route path="/register" component={Register} />
+				<Route path="/login" component={Login} />
+				<Route path="/logout" component={Logout} />
+				<Route path="/post/:slug" component={Single} />
 			</Switch>
 			<Footer />
 		</React.StrictMode>
